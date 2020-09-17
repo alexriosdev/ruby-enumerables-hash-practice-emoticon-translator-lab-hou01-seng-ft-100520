2f2565f8205346b3627162080f989167884ed954
Alejandro Rios <alexrios.dev@gmail.com>
2020-09-17 11:31:50 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,17 +3,13 @@ require "yaml"
 require "pry"
 
 def load_library(file_path)
-  # code goes here
-  
-  # library = YAML.load_file('lib/emoticons.yml')
- 
   library = YAML.load_file(file_path)
   new_hash = {}
   
-  library.each do |emoticon_name, value|
-    new_hash[emoticon_name] = {}
-    new_hash[emoticon_name][:english] = value[0]
-    new_hash[emoticon_name][:japanese] = value[1]
+  library.each do |key, value|
+    new_hash[key] = {}
+    new_hash[key][:english] = value[0]
+    new_hash[key][:japanese] = value[1]
   end
   new_hash
   
