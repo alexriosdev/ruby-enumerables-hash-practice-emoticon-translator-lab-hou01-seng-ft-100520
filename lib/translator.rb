cea90e9c60a2a0f5b304ea6bea13445fc60c487b
Alejandro Rios <alexrios.dev@gmail.com>
2020-09-17 11:28:57 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,13 +12,10 @@ def load_library(file_path)
   
   library.each do |emoticon_name, value|
     new_hash[emoticon_name] = {}
-    new_hash[emoticon_name] = value[0]
-    
-  
-  
-  
-  
-
+    new_hash[emoticon_name][:english] = value[0]
+    new_hash[emoticon_name][:japanese] = value[1]
+  end
+  new_hash
   
 end
 
